8dcd7d3baa8b4f63212f23c2d93cd9df7b086116
kairung <109464720+kairung@users.noreply.github.com>
2022-10-28 11:32:53 +0700

    . commit

--- Home.py
+++ Home.py
@@ -4,3 +4,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
+
+html_8 = """
+<div style="background-color:#6BD5DA;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>การทำนายข้อมูลดอกไม้</h5></center>
+</div>
+"""
+st.markdown(html_8, unsafe_allow_html=True)
+st.markdown("")
+
+dt=pd.read_csv("./data/iris.csv")
+st.write(dt.head(10))
+dt1 = dt['petal.length'].sum()
+dt2 = dt['petal.width'].sum()
+dt3 = dt['sepal.length'].sum()
+dt4 = dt['sepal.width'].sum()
+dx = [dt1, dt2, dt3, dt4]
+dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
+if st.button("แสดงการจินตทัศน์ข้อมูล"):
+   st.pie_chart(dx2)
+   st.button("ไม่แสดงข้อมูล")
+else:
+    st.write("ไม่แสดงข้อมูล")
+
+
