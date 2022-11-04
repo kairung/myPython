2c4035bd49b431803caae1ece714464bdc75e3f0
kairung <109464720+kairung@users.noreply.github.com>
2022-11-04 10:33:28 +0700

    .commit

--- Home.py
+++ Home.py
@@ -46,17 +46,17 @@ sp_wd=st.number_input("กรุณาเลือกข้อมูล sepal.wi
 
 
 if st.button("ทำนายผล"):
-   st.markdown("ใส่โมเดล")
+    st.markdown("ใส่โมเดล")
+    loaded_model = pickle.load(open('./data/trained_model.sav', 'rb'))
+    input_data = (pt_len, pt_wd, sp_len, sp_wd)
+    # changing the input_data to numpy array
+    input_data_as_numpy_array = np.asarray(input_data)
+    # reshape the array as we are predicting for one instance
+    input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
+    prediction = loaded_model.predict(input_data_reshaped)
+    st.write(prediction)
    st.button("ไม่แสดงข้อมูล")
 else:
     st.write("ไม่แสดงข้อมูล")
 
 
-loaded_model = pickle.load(open('./data/trained_model.sav', 'rb'))
-input_data = (pt_len, pt_wd, sp_len, sp_wd)
-# changing the input_data to numpy array
-input_data_as_numpy_array = np.asarray(input_data)
-# reshape the array as we are predicting for one instance
-input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
-prediction = loaded_model.predict(input_data_reshaped)
-st.write(prediction)
